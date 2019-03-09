608b29af2e3238cd31f95b0ec7fb070311e24f87
Anders Lindén <purist.coder@gmail.com>
2019-03-09 12:34:21 +0100

    Registering capability handler in common proxys preinit.

--- main/java/proxies/CommonProxy.java
+++ main/java/proxies/CommonProxy.java
@@ -1,7 +1,9 @@
 package proxies;
 
+import capabilities.CapabilityHandler;
 import capabilities.ISetHomePlayerLocations;
 import capabilities.SetHomePlayerStorage;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.capabilities.CapabilityManager;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -13,7 +15,9 @@ public class CommonProxy
     public void preInit(FMLPreInitializationEvent e)
     {
         CapabilityManager.INSTANCE.register(ISetHomePlayerLocations.class, new SetHomePlayerStorage(),
-                new SetHomePlayerStorage.Factory());        
+                new SetHomePlayerStorage.Factory());
+
+        MinecraftForge.EVENT_BUS.register(new CapabilityHandler());
     }
 
     public void init(FMLInitializationEvent e)
